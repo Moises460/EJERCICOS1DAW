aaa4a5f2a49303996980f4aadaa89b23d1ca7052
A609\1DAW <moiherort@alu.edu.gva.es>
2025-09-25 17:15:29 +0200

    SE AÑADE LA SUMA DE NUMEROS

--- PRUEBA1/src/main/java/org/example/Actividad2.java
+++ PRUEBA1/src/main/java/org/example/Actividad2.java
@@ -6,17 +6,29 @@ public class Actividad2 {
      static void main() {
 
          Scanner teclas = new Scanner(System.in);
-         System.out.println("Pon tu NicKname"); //esto es el esrcibir
 
+         System.out.println("Pon tu NicKname"); //esto es el esrcibir
          String nombre = teclas.nextLine();
 
 
          System.out.println("¿Cual es tu curso?"); //esto es el esrcibir
-
-
          String curso= teclas.next();
 
-          System.out.println("hola"+nombre+"Bienvenido a "+curso);
+         System.out.println("hola" + nombre +"Bienvenido a "+ curso);
+
+          System.out.println("escribe un numero");
+          int num1 = teclas.nextInt();
+
+          System.out.println("escribe un numero");
+          int num2 = teclas.nextInt();
+
+          System.out.println("el resultado es "+(num1+num2));
+
+          int resultado = num1+num2;
+
+          System.out.println("el resultado es"+ resultado);
+
+
 
 
     }
