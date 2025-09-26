51880fc7a333f4b70b05f2f20c6c5db8f250eb54
A609\1DAW <moiherort@alu.edu.gva.es>
2025-09-26 16:59:37 +0200

    no message

--- PRUEBA1/src/main/java/org.example/scratch.java
+++ PRUEBA1/src/main/java/org.example/scratch.java
@@ -1,22 +1,36 @@
-package org.example;
+
 import java.util.Scanner;
 
 
-class Scratch {
+class Bateriadepoblemas {
 
     static void main() {
 
+         final int jornada_sin_extras=40;
+         final double plus_tarifa=1.5;
 
         Scanner teclas = new Scanner(System.in);
 
         System.out.println("Pon tu NicKname");//esto es el esrcibir
-        String curso= teclas.next();
+        String NOMBRE= teclas.next();
 
         System.out.println("escribe las horas");
-        int num1 = teclas.nextInt();
+        double HORAS = teclas.nextDouble();
+
         System.out.println("escribe la tarifa");
-        int num2 = teclas.nextInt();
-        System.out.println("el resultado es "+(num1*num2));
+        double TARIFA = teclas.nextDouble();
+
+        double SALARIO;
+
+        if (HORAS > jornada_sin_extras) {
+            double horasExtras = HORAS - jornada_sin_extras;
+            SALARIO = jornada_sin_extras * TARIFA + horasExtras * TARIFA * plus_tarifa;
+        } else {
+            SALARIO  = HORAS * TARIFA;
+        }
+
+
+        System.out.println( NOMBRE + " tu salario es "+ SALARIO+"€");
 
 
 
