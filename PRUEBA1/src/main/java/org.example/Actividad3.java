2e704b1ecef655b89063e552f6ef845e8c88291a
A609\1DAW <moiherort@alu.edu.gva.es>
2025-09-30 21:17:33 +0200

    no message

--- PRUEBA1/src/main/java/org.example/Actividad3.java
+++ PRUEBA1/src/main/java/org.example/Actividad3.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Actividad3 {
     public static void main(String[] args) {
         Scanner teclas = new Scanner(System.in);
-
+        final int cero = 0;
         System.out.println("Introduce un numero:");
-        String num1 = teclas.next();
+        int numero = teclas.nextInt();
 
-        // Convertimos el String a int
-        int numero = Integer.parseInt(num1);
 
         // Ahora sí podemos compararlo
         if (numero < 0) {
             System.out.println("El número es negativo.");
         } else {
-            System.out.println("El número es positivo o cero.");
+            if (numero > 0) {
+                System.out.println("el numero es positivo");
+            } else {
+                System.out.println("El número es cero ");
+            }
         }
     }
 }
--- PRUEBA1/src/main/java/org.example/Actividad5.java
+++ PRUEBA1/src/main/java/org.example/Actividad5.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Actividad5 {
+
+    static void main() {
+        Scanner teclas= new Scanner(System.in);
+
+        int num1=0;
+        int num2=0;
+
+        try{
+        System.out.println("Introduce numero para divir");
+            num1 = teclas.nextInt();
+            num2 = teclas.nextInt();
+        }catch (Exception err){
+            System.out.println("INTRODUCE NUMERO!!!NO LETRAS,GRACIAS<3");
+        }
+       if(num2==0){
+           System.out.println("NO PUEDES DIVIR ENTRE 0!!!!");
+       }
+        int resultado = num1 / num2;
+       System.out.println("el resltado es " + resultado);
+    }
 }
