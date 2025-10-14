895f9f4921efb2bb8e417865b1a28337b90acc1e
A609\1DAW <moiherort@alu.edu.gva.es>
2025-10-14 21:18:03 +0200

    nuevo

--- PRUEBA1/src/main/java/org.example/Practica_7.java
+++ PRUEBA1/src/main/java/org.example/Practica_7.java
@@ -3,15 +3,38 @@ import java.util.Scanner;
 public class Practica_7 {
     static void main(){
         Scanner teclas= new Scanner(System.in);
-        int num1=433;
+        boolean repetir=false;
+        int num1=0;
+        int num2=0;
 
+            num1= teclas.nextInt();
+            num2= teclas.nextInt();
 
         String num_string= Integer.toString(num1);
         String num_izquierda = num_string.substring(0,1);
-        int num_izquierda_int= Integer.parseInt(num_izquierda);
+        String num_central = num_string.substring(1,2);
+        String num_derecha = num_string.substring(2,3);
 
-        
+        int num_izquierda_int= Integer.parseInt(num_izquierda);
+        int num_central_int= Integer.parseInt(num_central);
+        int num_derecha_int= Integer.parseInt(num_derecha);
+
+        String num_string2 = Integer.toString(num2);
+        String num_izquierda2 = num_string2.substring(0,1);
+        String num_central2 = num_string2.substring(1,2);
+        String num_derecha2= num_string2.substring(2,3);
+
+        int num_izquierda_int2= Integer.parseInt(num_izquierda2);
+        int num_central_int2= Integer.parseInt(num_central2);
+        int num_derecha_int2= Integer.parseInt(num_derecha2);
+
+        int resulado= num2*num1;
+
         System.out.println(num_izquierda_int*123);
+        System.out.println(num_izquierda_int);
+        System.out.println(num_izquierda_int2);
+
+
 
 
     }
---
+++ PRUEBA1/src/main/java/org.example/Prueba1.java
@@ -0,0 +1,56 @@
+public class Prueba1 {
+
+
+
+    static void main(){
+
+
+        String frase="Viva españa y su gente<3";
+
+        int tamanyo= frase.length();
+        System.out.println(tamanyo);
+
+        System.out.println(frase.charAt(17));
+
+        String trozo=frase.substring(14,24);
+        System.out.println(trozo);
+
+        String trozo2 =frase.substring(5);
+        System.out.println(trozo2);
+
+        int posicion_españa=frase.indexOf("españa");
+        System.out.println(posicion_españa);
+
+        String frase_mayus= frase.toUpperCase();
+        System.out.println(frase_mayus);
+        String frase_minus = frase.toLowerCase();
+        System.out.println(frase_minus);
+
+
+        String nombre="Moises             ";
+        System.out.println(nombre.trim() + " - " + nombre + "*");
+        String nombre_mayus=nombre.toUpperCase();
+
+        boolean iguales= nombre_mayus.trim().equals("MOISES");
+
+        System.out.println(iguales);
+
+        boolean iguales2=nombre.trim().equalsIgnoreCase("Moises");
+
+        System.out.println(iguales2);
+
+
+        String cambiado= frase.replace("españa","España");
+        System.out.println(cambiado);
+
+
+
+
+
+
+
+
+
+
+    }
+}
