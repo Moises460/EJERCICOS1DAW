fab315705745cef8bdb425d5f57689911d5b5d56
A609\1DAW <moiherort@alu.edu.gva.es>
2025-10-16 15:42:01 +0200

    no message

--- PRUEBA1/src/main/java/org.example/Bascula.java
+++ PRUEBA1/src/main/java/org.example/Bascula.java
@@ -5,35 +5,35 @@ public class Bascula {
     static void main() {
         Scanner teclas = new Scanner(System.in); //sacener de teclas
 
-       final double peso_max=120;      //Eston son las variables /constantes
-       final double altura_max=210;
-       final double altura_min=140;
-       double altura=0;
-       double peso=0;
+        final double peso_max = 120;      //Eston son las variables /constantes
+        final double altura_max = 210;
+        final double altura_min = 140;
+        double altura = 0;
+        double peso = 0;
 
-        boolean repetir= false;       //funcionamiento para el {Do {While
+        boolean repetir = false;       //funcionamiento para el {Do {While
         do {
 
-        try {                                                          //Linea de try catch- aki se comprueba que se ecriban valores numericos
-            System.out.println("BIENVENIDO AL RATON JUGUETON");
-            System.out.println("Leyendo altura  (cm)");
-             altura= teclas.nextInt();
-            repetir=false;
-        } catch (Exception err) {
-            System.out.println("porfavor NUMEORS NO LETRAS");
-             repetir=true;
-        }
-        teclas.nextLine();
+            try {                                                          //Linea de try catch- aki se comprueba que se ecriban valores numericos
+                System.out.println("BIENVENIDO AL RATON JUGUETON");
+                System.out.println("Leyendo altura  (cm)");
+                altura = teclas.nextInt();
+                repetir = false;
+            } catch (Exception err) {
+                System.out.println("porfavor NUMEORS NO LETRAS");
+                repetir = true;
+            }
+            teclas.nextLine();
 
-    }while (repetir==true);
+        } while (repetir == true);
 
-        double resultado1= altura_min - altura;                                     //Calculo de la altura minima
-        if (altura<altura_min){                                                    // estas son las lineas para hacer la comprobacion de altura
-            System.out.println("Lo sineto,no cumples la altura minima:( Te faltan "+ resultado1+"cm");
-        }else{
-            if (altura>altura_max){
+        double resultado1 = altura_min - altura;                                     //Calculo de la altura minima
+        if (altura < altura_min) {                                                    // estas son las lineas para hacer la comprobacion de altura
+            System.out.println("Lo sineto,no cumples la altura minima:( Te faltan " + resultado1 + "cm");
+        } else {
+            if (altura > altura_max) {
                 System.out.println("Lo sineto,superas la altura MAX");
-            }else{
+            } else {
                 do {
                     try {
                         System.out.println("Leyendo Peso (Kg)");
@@ -44,30 +44,30 @@ public class Bascula {
                         repetir = true;
                     }
                     teclas.nextLine();
-                }while ( repetir==true);
+                } while (repetir == true);
 
-                 double peso_min = altura * 2 / 8;                                               // Calculo del peso minimo
-                 double resltado2= peso-peso_max;
-<<<<<<< HEAD
+                double peso_min = altura * 2 / 8;                                               // Calculo del peso minimo
+                double resltado2 = peso - peso_max;
 
-                if (peso<peso_min){
-=======
-                if (peso<peso_min){                                                             //estan lineas son para hacer la comprobacion de peso
->>>>>>> d187c9f53de8d7946b25d3ad6fc853795c5e5836
-                    System.out.println("No puedes subir porque pesas menos de "+ peso_min);
-                } else{
-                    if (peso>peso_max){
-                        System.out.println("No puedes subir porque sobrepas  "+resltado2+"kg el limite de la atraccíon");
-                    }else{
-                        System.out.println("Pesas "+peso+" kg .El peso minimo calculado: "+peso_min);
-                        System.out.println("¡¡PUDES SUBIRTE!!");
+
+                if (peso < peso_min) {
+
+                    if (peso < peso_min) {                                                             //estan lineas son para hacer la comprobacion de peso
+                        System.out.println("No puedes subir porque pesas menos de " + peso_min);
+                    } else {
+                        if (peso > peso_max) {
+                            System.out.println("No puedes subir porque sobrepas  " + resltado2 + "kg el limite de la atraccíon");
+                        } else {
+                            System.out.println("Pesas " + peso + " kg .El peso minimo calculado: " + peso_min);
+                            System.out.println("¡¡PUDES SUBIRTE!!");
+                        }
                     }
+
+
                 }
-
-                
             }
+
+
         }
-
-
     }
 }
--- PRUEBA1/src/main/java/org.example/Practica3_tema2.java
+++ PRUEBA1/src/main/java/org.example/Practica3_tema2.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Practica3_tema2 {
+    static void main(){
+        Scanner teclas= new Scanner(System.in);
+        
+
+
+    }
 }
