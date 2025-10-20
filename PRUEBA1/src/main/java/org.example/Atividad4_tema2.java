caecfd63220685cfbda389e099a9ffa36d7338b6
A609\1DAW <moiherort@alu.edu.gva.es>
2025-10-20 21:05:21 +0200

    no message

--- PRUEBA1/src/main/java/org.example/Atividad4_tema2.java
+++ PRUEBA1/src/main/java/org.example/Atividad4_tema2.java
@@ -8,7 +8,7 @@ public class Atividad4_tema2 {
         final int AÑO_MIENTO = 1900;
 
         LocalDateTime fecha = LocalDateTime.now();
-        int año_actual=fecha.getYear();
+        int año_actual = fecha.getYear();
         int anyo_nacimiento_int = 0;
 
         System.out.println("ELIGE UN MODO");
@@ -18,56 +18,54 @@ public class Atividad4_tema2 {
 
         if (teclas.hasNextInt()) {
             modo = teclas.nextInt();
-
         } else {
             System.out.println("INTODUCE UN VALOR NUMERICO<3");
         }
-             switch (modo){
 
-                 case 1:
+        switch (modo) {
 
-                     System.out.println("Introduce tu año de nacimiento...");
-                     String anyo_nacimiento = teclas.next();
+            case 1:
 
-                     try{
-                         anyo_nacimiento_int = Integer.parseInt(anyo_nacimiento);
-                     }catch (Exception e) {
-                         System.out.println("El formato introducido para el año no es correcto.");
-                         return;
-                     }
-                 case 2:
-                     int edad = 0;
-                     System.out.println("Introduce tu edad...");
-                     if (teclas.hasNextInt()){
-                         edad = teclas.nextInt();
-                     }else{
-                         System.out.println("Formato incorrecto. No es numérico.");
-                         return;
-                     }
+                System.out.println("Introduce tu año de nacimiento...");
+                String anyo_nacimiento = teclas.next();
 
-                 default:
-                     System.out.println("El modo no existe!");
-                     return;
+                try {
+                    anyo_nacimiento_int = Integer.parseInt(anyo_nacimiento);
+                } catch (Exception e) {
+                    System.out.println("El formato introducido para el año no es correcto.");
+                    return;
+                }
+                break;
+            case 2:
+                int edad = 0;
+                System.out.println("Introduce tu edad...");
+                if (teclas.hasNextInt()) {
+                    edad = teclas.nextInt();
+                } else {
+                    System.out.println("Formato incorrecto. No es numérico.");
+                    return;
+                }
+                break;
 
-         }
+            default:
+                System.out.println("El modo no existe!");
+                return;
 
+        }
 
+           if (anyo_nacimiento_int >= AÑO_MIENTO && anyo_nacimiento_int <= año_actual) {
 
-
-
-        if (anyo_nacimiento_int >= AÑO_MIENTO && anyo_nacimiento_int <= año_actual){
-
-            if (anyo_nacimiento_int <= 1927){
+            if (anyo_nacimiento_int <= 1927) {
                 System.out.println("Generacion no bautizada");
-            } else if (anyo_nacimiento_int<= 1928 && anyo_nacimiento_int<= 1944) {
+            } else if (anyo_nacimiento_int <= 1928 && anyo_nacimiento_int <= 1944) {
                 System.out.println("Generación Silent");
-            } else if (anyo_nacimiento_int>=1945 && anyo_nacimiento_int <= 1964) {
+            } else if (anyo_nacimiento_int >= 1945 && anyo_nacimiento_int <= 1964) {
                 System.out.println("Baby Boomer");
-            } else if (anyo_nacimiento_int>=1965 && anyo_nacimiento_int<=1981) {
+            } else if (anyo_nacimiento_int >= 1965 && anyo_nacimiento_int <= 1981) {
                 System.out.println("Generación X");
-            } else if (anyo_nacimiento_int>= 1982 && anyo_nacimiento_int<=1994) {
+            } else if (anyo_nacimiento_int >= 1982 && anyo_nacimiento_int <= 1994) {
                 System.out.println("Millenials la mejor");
-            }else{
+            } else {
                 System.out.println("Generación Z");
             }
 
@@ -75,3 +73,4 @@ public class Atividad4_tema2 {
 
     }
 }
+
