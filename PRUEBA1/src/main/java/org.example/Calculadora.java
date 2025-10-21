4ad622398c698837c377cb6cb4b3663290d10532
A609\1DAW <moiherort@alu.edu.gva.es>
2025-10-21 19:54:18 +0200

    no message

--- PRUEBA1/src/main/java/org.example/Calculadora.java
+++ PRUEBA1/src/main/java/org.example/Calculadora.java
@@ -1,21 +1,144 @@
+import com.sun.source.tree.WhileLoopTree;
+
 import java.util.Scanner;
 
 public class Calculadora {
     static void main() {
         Scanner teclas= new Scanner(System.in);
-        double modo=0;
+        int modo;
         double num1=0;
         double num2=0;
+        boolean repetir=true;
 
-        System.out.println("SELECIONE QUE QUIERE HACER");
-        System.out.println("---------------------------");
-        System.out.println("[+]-1");
-        System.out.println("[-]-2");
-        System.out.println("[x]-3");
-        System.out.println("[/]-4");
-        System.out.println("[√]-5");
-        System.out.println("--------------------------");
+            System.out.println("--------CALCULADORA--------");
+            System.out.println("SELECCIONE QUE QUIERE HACER");
+            System.out.println("-----------------------------");
+            System.out.println("[+] - 1");
+            System.out.println("[-] - 2");
+            System.out.println("[x] - 3");
+            System.out.println("[/] - 4");
+            System.out.println("[√] - 5");
+            System.out.println("[salir] - 6");
+            System.out.println("-----------------------------");
 
+            if (teclas.hasNextInt()) {
+                modo = teclas.nextInt();  // ✅ lee el número correctamente
+            } else {
+                System.out.println(" INTRODUCE UN NÚMERO, NO LETRAS <3");
+                teclas.next(); // descarta entrada no válida
+                 return;
+            }
+
+            switch (modo) {
+                case 1:
+                    System.out.println("HAS ELEGIDO LA SUMA");
+                    System.out.println("INTRODUCE NÚMEROS PARA SUMAR");
+
+                    if (teclas.hasNextDouble()) {
+                        num1 = teclas.nextDouble();
+                        System.out.println("INTRODUCE OTRO NUMERO PARA SUMAR");
+
+                        if (teclas.hasNextDouble()) {
+                             num2 = teclas.nextDouble();
+                            double suma = num1 + num2;
+                            System.out.println("LA SUMA DE " + num1 + " + " + num2 + " = " + suma);
+                        } else {
+                            System.out.println("EL SEGUNDO VALOR NO ES UN NÚMERO ");
+                        }
+                    } else {
+                        System.out.println("EL PRIMER VALOR NO ES UN NÚMERO ");
+                    }
+                    break;
+                case 2:
+                    System.out.println("HAS ELEGIDO LA RESTA");
+                    System.out.println("INTRODUCE NUMEROS PARA RESTAR");
+
+                    if(teclas.hasNextDouble()){
+                        num1= teclas.nextDouble();
+                        System.out.println("INTRODUCE OTRO NUEMRO PARA RESTAR");
+                        if (teclas.hasNextDouble()) {
+                            num2 = teclas.nextDouble();
+                            double resta = num1 - num2;
+                            System.out.println("LA RESTA DE " + num1 + " - " + num2 + " = " + resta);
+                        }else {
+                            System.out.println("EL SEGUNDO VALOR NO ES UN NÚMERO");
+                        }
+                    }else {
+                        System.out.println("EL PRIMER VALOR NO ES UN NÚMERO");
+                    }
+                    break;
+                case 3:
+                    System.out.println("HAS ELEGIDO LA MULTIPLICACIÓN");
+                    System.out.println("INTRODUCE NUMEROS PARA MULTIPLICAR");
+
+                    if(teclas.hasNextDouble()){
+                        num1= teclas.nextDouble();
+                        System.out.println("INTRODUCE OTRO NUEMRO PARA MULTIPLICAR");
+
+                        if(teclas.hasNextDouble()) {
+                            num2 = teclas.nextDouble();
+                            double multi = num1 * num2;
+                            System.out.println("LA MULTIPLICACION DE " + num1 + " x " + num2 + " = " + multi);
+                        }else {
+                            System.out.println("EL SEGUNDO VALOR NO ES UN NÚMERO");
+                        }
+                    }else {
+                        System.out.println("EL PRIMER VALOR NO ES UN NÚMERO");
+                    }
+                    break;
+                case 4:
+                    System.out.println("HAS ELEGIDO LA DIVISIÓN");
+                    System.out.println("INTRODUCE UN NUEMRO PARA DIVIR");
+
+                    if(teclas.hasNextDouble()){
+                        num1= teclas.nextDouble();
+                        System.out.println("INTRODUCE OTRO NUMERO PARA DIVIDIR(QUE NO SEA 0)");
+                        if(teclas.hasNextDouble()) {
+                            num2 = teclas.nextDouble();
+                        }else {
+                            System.out.println("EL SEGUNDO VALOR NO ES UN NÚMERO");
+                        }
+                    }else {
+                        System.out.println("EL PRIMER VALOR NO ES UN NÚMERO");
+                    }
+                    if (num1 == 0 || num2==0) {
+                        System.out.println("NO SE PUEDE DIVIDIR ENTRE 0");
+                        return;
+                    } else {
+                        double division = num1/num2;
+                        System.out.println("LA DIVISION DE "+num1+" / "+num2+" = "+division);
+                    }
+                    break;
+                case 5:
+                    System.out.println("HAS ELEGIDO LA RAÍZ");
+                    System.out.println("INTRODUCE UN NUMERO PARA REALIZAR LA RAIZ");
+
+                    if(teclas.hasNextDouble()){
+                        num1= teclas.nextDouble();
+                        if(num1<0){
+                            System.out.println("NO SE PUEDE HACER RAICES DE NUMEROS NEGATIVOS");
+                        }else {
+                            double raiz = Math.sqrt(num1);
+                            System.out.println("LA RAIZ CUADRADA DE  " + num1 + " ES " + raiz);
+                        }
+                    }else {
+                        System.out.println("SOLO NUMEROS<3");
+                    }
+                    break;
+                case 6:
+                    System.out.println("Saliendo...");
+                    System.out.println("GRACIAS POR SU VISITA A LA MEJOR CALCULADORA");
+                    break;
+
+                default:
+                    System.out.println("ELIGE UNA OPCION VALIDA (1-6) <3");
+
+
+            }
+
+
+
+<<<<<<< HEAD
         if (teclas.hasNext()){
             modo= teclas.nextDouble();
         }else{
@@ -23,5 +146,7 @@ public class Calculadora {
 
         }
         System.out.println(modo);
+=======
+>>>>>>> e0de866eeec4925403f140a0f4e74e54c7b56931
     }
 }
