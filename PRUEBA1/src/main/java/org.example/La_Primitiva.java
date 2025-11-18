d1f497277c20ebe1223541047583d9ff41e55f6f
AYAZ\ayaz1 <moiherort@alu.edu.gva.es>
2025-11-18 09:36:15 +0100

    no message

---
+++ PRUEBA1/src/main/java/org.example/La_Primitiva.java
@@ -0,0 +1,142 @@
+package org.example;
+
+import java.lang.String;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class La_Primitiva {
+    static void main() {
+        Scanner tc = new Scanner(System.in);
+        Random rnd = new Random();
+
+        String usuario;
+        String[] usuario_val;
+
+        boolean control = true;
+        int[] premio = new int[6];
+        int reintegro= rnd.nextInt(10);
+        int complementario= rnd.nextInt(50);
+
+        for (int i = 0; i < premio.length; i++) {
+            premio[i] = rnd.nextInt(49) + 1;
+            for (int j = 0; j < i; j++) {
+                if (premio[i] == premio[j]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+        Arrays.sort(premio);
+
+
+        System.out.println("*-------------------------------------------------------*");
+        System.out.println("               BIENVENIDO A LA PRIMITIVA");
+        System.out.println("*-------------------------------------------------------*");
+
+        do {
+
+            System.out.println("INTRODUCE SUS NUMEROS PARA LA PRIMITIVA [N-N-N-N-N-N/R]");
+            usuario = tc.nextLine();
+            usuario_val = usuario.split("[-/]");
+
+            if (!usuario.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}/\\d{1}")) {
+                System.out.println("FORMATO INCORRECTO EJ:[1-2-3-4-5-6/7]");
+                control = false;
+            } else {
+                control = true;
+
+                for (int i = 0; i < 6; i++) {
+                    int correcto = Integer.parseInt(usuario_val[i]);
+
+                    if (correcto < 1 || correcto > 49) {
+                        System.out.println("NO PUEDES INTRODUCIR NÚMEROS MAYORES DE 49 NI MENORES DE 1");
+                        control = false;
+                        break;
+                    }
+
+                    for (int j = i + 1; j < 6; j++) {
+                        int comprobador = Integer.parseInt(usuario_val[j]);
+                        if (correcto == comprobador) {
+                            System.out.println("NO PUEDES REPETIR NÚMEROS EN LA PRIMITIVA");
+                            control = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+        } while (!control);
+
+        int aciertos = 0;
+        int acierto_reintegro=0;
+        int acierto_complementario=0;
+        int[] usuario_nums = new int[6];
+
+        for (int i = 0; i < usuario_nums.length; i++) {
+            for (int j = 0; j < premio.length; j++) {
+                if (usuario_nums[i] == premio[j]) {
+                    aciertos++;
+                }
+
+            }
+        }
+
+
+        for (int i = 0; i < usuario_nums.length; i++) {
+            if (usuario_nums[i] == complementario) {
+                acierto_complementario++;
+                break;
+            }
+        }
+
+
+
+
+        int posicion_final = Integer.parseInt(usuario_val[usuario_val.length - 1]);
+        if(reintegro ==posicion_final ){
+            acierto_reintegro++;
+        }
+
+
+        System.out.println("*---------------------------*");
+        System.out.println("HA SALIDO:");
+        System.out.println( Arrays.toString(premio));
+        System.out.println("COMPLEMENTARIO: "+complementario);
+        System.out.println("REINTREGO: "+reintegro);
+        System.out.println("*---------------------------*");
+
+        System.out.println("RESULTADO");
+
+
+        switch (aciertos){
+            case 6:
+                System.out.println("1ª CATEGORIA: 6 NÚMEROS ACERTADOS");
+                break;
+            case 5:
+                if (acierto_complementario == 1) {
+                    System.out.println("2ª CATEGORIA: 5 NÚMEROS ACERTADOS + COMPLEMENTARIO");
+                }else {
+                    System.out.println("3ª CATEGORIA: 5 NÚMEROS ACERTADOS");
+                }
+                break;
+            case 4:
+                System.out.println("4ª CATEGORIA: 4 NÚMEROS ACERTADOS");
+                break;
+            case 3:
+                System.out.println("5ª CATEGORIA: 3 NÚMEROS ACERTADOS");
+                break;
+
+            default:
+                if (acierto_reintegro == 1) {
+                    System.out.println("REINTREGO ACERTADO");
+                } else {
+                    System.out.println("NO PREMIADO");
+                }
+                break;
+
+        }
+        System.out.println("*---------------------------*");
+
+    }
+}
